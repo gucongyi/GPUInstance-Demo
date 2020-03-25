211e0990ff294537b34b88c7ed62ca3830c9895a
gucongyi <gucongyii@163.com>
2020-03-25 17:46:13 +0800

    脚本中//使用MaterialPropertyBlock来修改shader的实例化变化的值

---
+++ GPUInstanceDemo/Assets/Scripts/TestMaterials.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMaterials : MonoBehaviour
+{
+    public GameObject goPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10000; i++)
+        {
+            GameObject go = GameObject.Instantiate(goPrefab);
+            go.transform.parent = transform;
+            int randomInt = i;
+            randomInt = Mathf.Clamp(randomInt, 1, 5);
+            //使用MaterialPropertyBlock来修改shader的实例化变化的值
+            MaterialPropertyBlock block = new MaterialPropertyBlock();
+            int brightnessID = Shader.PropertyToID("_Brightness");
+            block.SetFloat(brightnessID, randomInt);
+            go.GetComponent<MeshRenderer>().SetPropertyBlock(block);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
